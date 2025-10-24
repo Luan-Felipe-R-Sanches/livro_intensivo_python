bab10066aa357ce1948b0e518f98eb186bc966a5
Luan-Felipe-R-Sanches <luanfeliperodrigues25@gmail.com>
2025-10-24 09:35:39 -0400

    dicionario

---
+++ python_work/amesument_park.py
@@ -0,0 +1,7 @@
+age = 12
+if age < 4:
+    print("Your admission is $0.")
+elif age < 18:
+    print("Your admission is $25.")
+else:
+    print("Your admission is $40.")
--- python_work/toppings.py
+++ python_work/toppings.py
@@ -1,4 +1,7 @@
-requested_topping = 'mushrooms'
+requested_toppings = ['mushrooms', 'extra cheese', 'pepperoni']
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers right now.")
+    print(f"Adding {requested_topping}.")
 
-if requested_topping != 'anchovies':
-    print("Hold the anchovies!")
+print("\nFinished making your pizza!")
